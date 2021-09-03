d17e53fa5ebabe4204ed38f79918491898f86ab2
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-09-03 18:51:30 +0530

    Create Reverse String.py

---
+++ Reverse String.py
@@ -0,0 +1,9 @@
+#One liner for python
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
+        
